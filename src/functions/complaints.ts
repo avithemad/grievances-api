ceb16c3e88518623b74c3b12aaaea3bac57862a3
Avinash Jayakar <avithemad@Dhananjayarajs-MacBook-Pro.local>
2022-08-19 22:46:44 +0530

    Add cors headers

--- src/functions/complaints.ts
+++ src/functions/complaints.ts
@@ -5,20 +5,33 @@ const handler: Handler = async (event, context) => {
   // your server-side functionality
 
   const complaintRepo = new GoogleComplaintsRepository();
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST",
+    "content-type": "application/json",
+  };
   switch (event.httpMethod) {
     case "GET":
       return {
         statusCode: 200,
+        headers,
         body: JSON.stringify(await complaintRepo.getComplaints()),
       };
     case "POST":
       return {
         statusCode: 201,
+        headers,
         body: JSON.stringify(
           await complaintRepo.addComplaint(
             JSON.parse(event.body ? event.body : "")
           )
         ),
+      };
+    case "OPTIONS":
+      return {
+        statusCode: 204,
+        headers,
       };
     default:
       return {
